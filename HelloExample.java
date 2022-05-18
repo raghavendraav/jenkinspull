35da5f1e07cbef3f582e4f0a4c9453c325891393
Raghavendra Anantha <raghavendra.anantha@gmail.com>
2022-05-18 10:11:27 +0530

    Update HelloExample.java

--- HelloExample.java
+++ HelloExample.java
@@ -2,7 +2,7 @@ public class HelloExample {
     public static void main(String[] s){
         for(int i=0; i<=10; i++) {
             System.out.println(i);
-            System.out.println("Done");
+            System.out.println("Update");
         }
     }
 }
